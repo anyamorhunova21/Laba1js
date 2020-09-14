8dd163cea9eeb8cd376c0c63678dc8a8b7224513
anyamorhunova <fyz12vjhueyjdf@gmail.com>
2020-09-14 22:00:55 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("aaaaaaa")
+function massive(n) {
+    let mas = []
+    for (let i = 0; i < n; i++) {
+        mas.push(i)
+    }
+    return mas
+}
+console.log(massive(5))
+
+
+
+function divide(x, y, z) {
+    const bool = x % y === 0 && x % z === 0;
+    console.log(bool)
+}
+divide(12, 6, -6)
+
+function capital(a) {
+    var symbol = a.length >= 1 && a.length <= 10 ? a[0].toUpperCase() + a.slice(1) : Error('Your string must be from 1 to 10 symbols!')
+    console.log(symbol)
+}
+capital("helloworld")
