b58b3e70a8203478ee989ac232d42bf126497ab7
anyamorhunova <fyz12vjhueyjdf@gmail.com>
2020-09-14 22:06:26 +0300

    commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("aaaaaaa")
+//1st task
 function massive(n) {
     let mas = []
     for (let i = 0; i < n; i++) {
@@ -9,13 +9,14 @@ function massive(n) {
 console.log(massive(5))
 
 
-
+//2nd task
 function divide(x, y, z) {
     const bool = x % y === 0 && x % z === 0;
     console.log(bool)
 }
 divide(12, 6, -6)
 
+//3rd task
 function capital(a) {
     var symbol = a.length >= 1 && a.length <= 10 ? a[0].toUpperCase() + a.slice(1) : Error('Your string must be from 1 to 10 symbols!')
     console.log(symbol)
